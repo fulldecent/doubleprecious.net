dc2668df0fa0462dbfe96022472b9ac97a9cc1fa
William Entriken <github.com@phor.net>
2020-05-23 01:22:36 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,250 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Double Precious</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="//cdn.jsdelivr.net/jquery.cookie/1.3.1/jquery.cookie.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Monsieur+La+Doulaise' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+    body{background:#fff5fa}
+    h1{font-family: 'Monsieur La Doulaise', cursive; font-size: 60px}
+.gold, a:link{
+    color: #D5AD6D;
+        
+    background: -webkit-linear-gradient(transparent, transparent),
+                -webkit-linear-gradient(top,
+            rgba(213,173,109,1) 0%, rgba(213,173,109,1) 26%,
+            rgba(226,186,120,1) 35%, rgba(163,126,67,1) 45%,
+            rgba(145,112,59,1) 61%, rgba(213,173,109,1) 100%);
+
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}    
+    </style>    
+  </head>
+  <body>
+    <div class="container">
+      <div class="page-header">
+        <img class="pull-right img-rounded" alt="the proposal" src="propose2.jpg" style="height:150px; width:150px" data-toggle="tooltip" data-placement="bottom" title="Print this out for your wallet">
+
+        <h1><span style="white-space: nowrap">Su Wang</span> &amp; <span style="white-space: nowrap">William Entriken, junior</span></h1>
+        <p class="lead">Wedding information for guests, -1 days to go</p>
+        <ul class="nav nav-pills">
+          <li><a href="#itinerary">&raquo; Itinerary, Directions, Hotels</a></li>
+          <li><a href="#proposal">&raquo; About us, Photos</a></li>
+          <li><a href="#party">&raquo; Wedding Party</a></li>
+          <li><a href="#registry">&raquo; Registry</a></li>
+        </ul>        
+      </div>
+     
+      <h3>Videos</h3>
+      <p class="lead">Videos from our wedding:
+        <ul>
+          <li><a href="https://www.youtube.com/watch?v=LHrjjFUGsmk">US Wedding 18:00 minutes</a>
+          <li><a href="https://www.youtube.com/watch?v=3loNAC189oE">Main China video 4:29</a> (<a href="https://www.youtube.com/watch?v=1NDzs8fTvLE">copy</a>)
+          <li><a href="https://www.youtube.com/watch?v=lBfXvhexM60">China: getting ready for big day (NSFW) 4:05</a>
+          <li><a href="https://www.youtube.com/watch?v=8lBgYAareko">China: intro to ceremony 3:51</a>
+          <li><a href="https://www.youtube.com/watch?v=WSJwwyZLtpQ">China: toasting 3:30</a>
+        </ul>
+      </p>
+
+      <h3 id="itinerary">The Itinerary</h3>
+      <p class="lead">Our wedding was held at <strong>Belle Voir Mannor</strong> on <strong>Friday August 15th, 2014</strong></p>
+      <address>
+        <i class="fa fa-compass pull-left fa-3x gold"></i>
+        <a href="https://goo.gl/maps/ajqfL" target="_blank">1601 State Rd, Bensalem, PA 19020</a><br>
+        (215) 633-0600
+      </address>
+      <dl class="dl-horizontal">
+        <dt>Friday 6:00pm</dt>
+        <dd>Ceremony begins</dd>
+        <dt>6:30pm</dt>
+        <dd>Cocktail hour</dd>
+        <dt>7:30pm</dt>
+        <dd>Reception</dd>
+<!--
+        <dt>Saturday 11:00am</dt>
+        <dd>Brunch for out-of-town guests</dd>
+-->        
+        <dt>Saturday 2:00pm</dt>
+        <dd>BBQ pool party at Bill's house, 410 Keats Road 19006</dd>
+        <dt>Sunday</dt>
+<!--        
+        <dd><a href="http://rivercountry.net" target="_blank">Tubing at Point Pleasant PA</a>, weather permitting</dd>
+-->        
+        <dd>Fun events to follow, such as tubing</dd>
+      </dl>
+      <div class="row">
+        <div class="col-md-4">
+          <h4>From I-95 North and South</h4>
+          <p>Take exit #35, RT 63 (Woodhaven Road). Take RT 63 to first exit, RT 13 (Bristol Pike). At traffic light at RT 13, turn left and proceed 1/2 mile to STATION AVE. Turn right and proceed 1/2 mile to STATE RD. Turn right and follow 2/10 mile to PEN RYN MANSION on the left (four cream-colored columns).</p>
+        </div>
+        <div class="col-md-4">
+          <h4>From South Jersey-Atlantic City Area</h4>
+          <p>Take ATLANTIC CITY EXPRESSWAY (North) to WALT WHITMAN BRIDGE. Take I-95 (North) and exit at RT 63 (Woodhaven Road). Take RT 63 to first exit, RT 13 (Bristol Pike). Follow <em>I-95 Directions</em>.</p>
+        </div>
+        <div class="col-md-4">
+          <h4>From New York and North Jersey</h4>
+          <p>Take NJ Turnpike (South) to EXIT 6. Follow PA Turnpike to EXIT 358. Take 1st exit, RT 13 SOUTH, for 6.5 miles to STATION AVE (cemetery on right). Turn left onto STATION AVE. Proceed 1/2 mile to STATE RD. Turn right and follow 2/10 mile to PEN RYN MANSION on the left (four cream-colored columns).</p>
+        </div>
+      </div>
+      
+      <h3>Hotels</h3>
+      <div class="row">
+        <div class="col-md-6">
+          <h4>Red Roof Inn - $74</h4>
+          <p><a href="https://goo.gl/maps/xIcdT" target="_blank">3100 Lincoln Highway, Feasterville, PA 19053</a><br>215-244-9422<br>Block code: B185WANENT</p>
+        </div>
+        <div class="col-md-6">
+          <h4>Holiday Inn Express - $109</h4>
+          <p><a href="https://goo.gl/maps/q1Dnu" target="_blank">1329 Bristol Pike Bensalem, PA 19020</a><br>215-245-5222<br><a href="http://ichotelsgroup.com/redirect?path=rates&amp;brandCode=EX&amp;regionCode=1&amp;localeCode=en&amp;GPC=WEW&amp;hotelCode=PNEEX&amp;_PMID=99801505">Block code: WANG-ENTRIKEN</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <h3>Double Precious</h3>
+      <p class="pull-right">
+        <span style="font-size:50pt">宝</span>
+        <span style="font-size:38pt; color:#655">宝</span>
+      </p>
+      <p class="lead">Not like other couples, Will always give me "weird" nicknames, such as Lucy, Yolanda, Princess, Su Baby, etc. and Little Precious is the one of them.
+      </p>
+      <p> "Precious" is written “宝” in Chinese. Almost all Chinese characters are created from "simple pictures" and means something, this character means "hidden treasure". And apparently, we are the treasures to each other. We cherish and support each other and are always happy together. And since I am short, of course, I am that "little" precious, and Will is that "big" precious. "Double precious" is our attitude of our love. 
+      </p>
+      <p><i class="fa fa-glass gold"></i> Please stop by at bar on the wedding day to try our signature cocktail "Double Precious". <span class="label label-success">PROTIP: order two</span></p>
+
+      <hr>
+
+      <h3 id="proposal">Our Romantic Proposal</h3>
+      <p class="lead">Will proposed to me in China, my beloved home country on 11/17/2013. That was a total shock. Will didn't give me any hint ahead of time.</p>
+      <p>The night before our trip, Will packed all his stuff and went to bed super early since his flight is about 5am the next morning. I was in my room and packing one check-in luggage and one carry-on luggage. Time went by and around 1am, I still had lots of stuff that can't be fit into the luggage. So I opened his luggage and moved stuff around. Who knows, the proposal ring was actually sitting in his "giant" shoe.
+      </p>
+      <p>When we got to China the first night, he asked me to cancel my dinner plan with friends because he wanted to spend one night with just me in Shanghai, our favorite city in China. We had the dinner at a fancy restaurant, and then he proposed to go to <a href="https://en.wikipedia.org/wiki/Shanghai_World_Financial_Center" target="_blank">Shanghai world financial center</a>, the supertall skyscraper. This is a very famous building and won lots of awards for architecture. We got there and I noticed that the building has the world's tallest observatory deck. Of course, I told Will that I have to get on the top. The entire building looks so fancy. We got on to the 100th floor and the view was spectacular  Will was also very nice to me. Besides my only selfie pictures, he finally agreed to take pictures for me in different poses. I felt I was living in the heaven. After a while, Will said: "Su, let us find someone to take pictures for us". "Sure, I got someone". I found a Western couple as well and handed them the phone. Here is where the "weirdness" kicked in. Will said to them "we will make some movements, please just keep taking pictures." I was wondering--hmmm, what movements?? Then we did a few stupid poses, Will said "show me your surprised face." I tried my best with the surprised faces, but he still said: "your surprised faces are terrible."  "I think my surprised face is awesome, plus there is really nothing that I can surprise for..." While I was talking, he kneeled down and reached out his pocket. "OMG!!!!!" I can hear my heartbeat at that moment. He took out the ring and opened in front of me, but didn't say anything. I was thinking: hmm..so..do I need to say anything first? but it is not what is showing in the movie??. So I asked him "what should I do now?". Ha! Then he said in Chinese to me: "Will you marry me?". That was definitely the historical moment. I said "YES" without even hesitating. Now I really think I should let him wait there at least for a few second. Bad me. And since his slow motion, which he did on purpose, we also got a few great proposal pictures.
+      </p>
+      <div class="row">
+        <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="Why does he take some many photos of me?">
+          <img class="img-rounded" alt="the proposal" src="propose1.jpg">
+        </div>
+        <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="I think my surprised face is awesome">
+          <img class="img-rounded" alt="the proposal" src="propose2.jpg">
+        </div>
+        <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="so..do I need to say anything first">
+          <img class="img-rounded" alt="the proposal" src="propose3.jpg">
+        </div>
+        <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="I said YES without even hesitating">
+          <img class="img-rounded" alt="the proposal" src="propose4.jpg">
+        </div>
+      </div>
+      <hr>
+
+      <h3 id="party">Wedding Party</h3>
+      <p class="lead">We look forward to sharing not just our Wedding Day but the rest of our lives with these people. They have a huge role in our day and they deserve to be on a pedestal. Thank you in advance to our wedding party, you have a very special place in our hearts!</p>
+      <h4>Su's attendants</h4>
+      <div class="row">
+        <div class="col-md-3" data-toggle="tooltip" data-placement="bottom" title="A romantic dinner for two, but she's looking at someone else">
+          <h5>Cao Wang, <br><span class="text-muted">Maid of Honor</span></h5>
+          <img alt="cathy" src="cathy.jpg" class="img-responsive img-rounded">
+        </div>
+        <div class="col-md-9">
+          <div class="row">
+            <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="This is how we dress in Canada">
+              <h5>Yiming Ouyang, <br><span class="text-muted">Bridesmaid</span></h5>
+              <img alt="yiming" src="yiming.jpg" class="img-responsive img-rounded">
+            </div>
+            <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="I am VERY afraid of the sun and rain, I will die if my iphone dies">
+              <h5>Eva Wang, <br><span class="text-muted">Bridesmaid</span></h5>
+              <img alt="eva" src="eva.jpg" class="img-responsive img-rounded">
+            </div>
+            <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="Hope my baby's hair looks just like mine!">
+              <h5>Yuhan Zhang, <br><span class="text-muted">Bridesmaid</span></h5>
+              <img alt="yuhan" src="yuhan.jpg" class="img-responsive img-rounded">
+            </div>
+            <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="I learned about fake eyelashes from Su">
+              <h5>Babygirl Tomeo, <br><span class="text-muted">Jr Bridesmaid</span></h5>
+              <img alt="baby" src="babygirl.jpg" class="img-responsive img-rounded">
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <h4>William's attendants</h4>
+      <div class="row">
+        <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="Here's lookin at YOU">
+          <h5>Harry McCloskey, <br><span class="text-muted">Best Man</span></h5>
+          <img alt="harry" src="harry.jpg" class="img-responsive img-rounded">
+        </div>
+        <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="Check one two, one two">
+          <h5>Jason Dodd, <br><span class="text-muted">Groomsman</span></h5>
+          <img alt="jason" src="jason.jpg" class="img-responsive img-rounded">
+        </div>
+        <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="I had to edit his GF out of this photo">
+          <h5>Chris Nolan, <br><span class="text-muted">Groomsman</span></h5>
+          <img alt="chris" src="chris.jpg" class="img-responsive img-rounded">
+        </div>
+        <div class="col-md-3 col-sm-6" data-toggle="tooltip" data-placement="bottom" title="Wears a tux only once a year">
+          <h5>Kevin Spier, <br><span class="text-muted">Groomsman</span></h5>
+          <img alt="kevin" src="kevin.jpg" class="img-responsive img-rounded">
+        </div>
+      </div>
+
+      <hr>
+
+      <h3 id="registry">Our Registries</h3>
+      <div class="row">
+        <div class="col-md-4">
+          <a href="http://www1.macys.com/registry/wedding/guest/?registryId=6187125" target="_blank">
+            <img src="http://photos.macysinc.com/photos/photos/small/macys_on_white_se_20707_200_73.jpg" alt="Macy's">
+          </a>
+        </div>
+        <div class="col-md-4">
+          <a href="http://www.bedbathandbeyond.com/store/giftregistry/view_registry_guest.jsp?pwsToken=&eventType=Wedding&registryId=541289791&pwsurl=" target="_blank">
+            <img src="https://www.bedbathandbeyond.com/_assets/global/images/logo/logo_bbb.png" alt="Bed Bath and Beyond">
+          </a>
+        </div>
+        <div class="col-md-4">
+          <a href="http://www.amazon.com/gp/registry/wedding/2ZSVCBH25M5Y9" target="_blank">
+            <img src="http://library.corporate-ir.net/library/17/176/176060/mediaitems/93/a.com_logo_RGB.jpg" width=200 alt="Amazon">
+          </a>
+        </div>
+      </div>
+      
+      <hr>
+
+      <footer>
+        <p class="lead"><a href="http://imgur.com/KRremsI.jpg"><i class="fa fa-heart gold"></i></a> Su Wang <i class="fa fa-phone gold"></i> 267-307-3724</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <script>
+$(function(){
+  $('#rsvp').submit(function(){
+    $('#rsvp').hide();
+    document.cookie="didRSVP=1;expires=Sat, 16 May 2109 18:40:22 GMT; path=/";
+  });
+  if ($.cookie('didRSVP')=="1") {
+    $('#rsvp').hide();
+  }
+
+  $("[data-toggle='tooltip']").tooltip();
+});       
+
+    </script>
+  </body>
+</html>
